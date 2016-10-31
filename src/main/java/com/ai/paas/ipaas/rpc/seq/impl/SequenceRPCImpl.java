c5ae73064c391d85b49aad2424acb4f5041df9ec
DOUXF <douxf@asiainfo.com>
2016-10-31 11:46:31 +0800

    add zone

--- src/main/java/com/ai/paas/ipaas/rpc/seq/impl/SequenceRPCImpl.java
+++ src/main/java/com/ai/paas/ipaas/rpc/seq/impl/SequenceRPCImpl.java
@@ -1,10 +1,16 @@
 package com.ai.paas.ipaas.rpc.seq.impl;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
 
 import com.ai.paas.ipaas.i18n.ResBundle;
+import com.ai.paas.ipaas.i18n.TimeZoneEnum;
 import com.ai.paas.ipaas.rpc.api.seq.ISequenceRPC;
 import com.ai.paas.ipaas.rpc.api.vo.ApplyInfo;
 import com.ai.paas.ipaas.rpc.api.vo.ApplyResult;
@@ -41,6 +47,12 @@ public class SequenceRPCImpl implements ISequenceRPC {
 
 	@Override
 	public ApplyResult getSeq(ApplyInfo info) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		sdf.setTimeZone(LocaleContextHolder.getTimeZone());
+		String sd = sdf.format(new Date());
+		sdf.setTimeZone(TimeZone.getTimeZone(TimeZoneEnum.GMT.getZone()));
+		System.out.println(new Date());
+		System.out.println(sd);
 		return new ApplyResult("000000", rb.getMessage("ipaas.apply.sucess"));
 	}
 
